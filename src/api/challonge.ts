6b55574262a0a3d175b000e9dde49ed20dd364ba
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2026-06-07 12:24:57 +0800

    Hardcode API Redirect

--- src/api/challonge.ts
+++ src/api/challonge.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL ||
   'https://api-test-cmxie.duckdns.org';
-
-console.log('API_BASE_URL:', API_BASE_URL);
 
 const api = axios.create({
   baseURL: API_BASE_URL,
